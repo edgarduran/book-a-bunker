96e8b3a2c963c39e6046aa8342dbdc05cd067cc4
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-02-03 15:26:36 -0700

    Refactor Stores::BunkersController
    Refactored the stores::bunker controller to use current store
    Slightly refactored the views associated with those changes

---
+++ app/controllers/stores/stores_controller.rb
@@ -0,0 +1,14 @@
+class Stores::StoresController < ApplicationController
+  before_action :store_not_found
+
+  helper_method :current_store
+
+  def current_store
+    @current_store ||= Store.find_by(slug: params[:store])
+  end
+
+  def store_not_found
+    redirect_to root_path unless current_store
+  end
+
+end
